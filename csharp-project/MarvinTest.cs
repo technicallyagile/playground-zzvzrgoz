f3c234c85960abd5648ddf620ffbe7e300c592b8
technicallyagile <daniel.lynn@agile42.com>
2020-09-21 12:55:29 -0600

    Update and rename MartinTest.cs to MarvinTest.cs

---
+++ csharp-project/MarvinTest.cs
@@ -0,0 +1,28 @@
+using Answer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.IO;
+
+namespace AI
+{
+    [TestClass]
+    public class MarvinTest
+    {
+
+	[TestMethod]
+	public void VerifyCountAllStars() 
+	{
+		Marvin marvin = new Marvin();
+		string response = marvin.talk("Hello.");
+		Assert.AreEqual ("Whatever.", response);
+	}
+
+	[TestCleanup()]
+      	public void Cleanup()
+      	{		
+
+      	}
+
+    }
+}
